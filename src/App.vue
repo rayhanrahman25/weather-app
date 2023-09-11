4e88bf3cbf7feffaa1f2e18a8be7aee9db46c734
rayhanrahman25 <rayhanrahman880@gmail.com>
2023-09-11 23:47:45 +0600

    added: weather overview on city view page

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,12 @@
   >
     <SiteNavigation />
     <RouterView />
-    <HomeView/>
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
 import SiteNavigation from "./components/SiteNavigation.vue";
-import HomeView from "./components/HomeView.vue";
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,36 +1,86 @@
 <template>
-    <div>
-
+    <!-- Banner -->
+    <div class="flex flex-col flex-1 items-center">
+        <div v-if="route.query.preview" class="text-white p-4 bg-weather-secondary w-full text-center">
+            <p>
+                You're currently previewing this city, click "+" icon to track the city
+            </p>
+        </div>
     </div>
+    <!-- Weather Overview -->
+    <div class="flex flex-col items-center text-white py-12">
+        <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+        <p class="text-sm mb-12">
+            {{
+          new Date(weatherData.currentTime).toLocaleDateString(
+            "en-us",
+            {
+              weekday: "short",
+              day: "2-digit",
+              month: "long",
+            }
+          )
+        }}
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString(
+            "en-us",
+            {
+              timeStyle: "short",
+            }
+          )
+        }}
+        </p>
+        <p class="text-8xl mb-8">
+            {{ Math.round(weatherData.current.temp) }}&deg;
+        </p>
+        <p>
+            Feels like
+            {{ Math.round(weatherData.current.feels_like) }} &deg;
+        </p>
+        <p class="capitalize">
+        {{ weatherData.current.weather[0].description }}
+        </p>
+        <img
+        class="w-[150px] h-auto"
+        :src="
+          `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
+        "
+        alt=""
+      />
+    </div>
+
+    <hr class="border-white border-opacity-10 border w-full"/>
+    <!-- Hourly Weather -->
 </template>
 
 <script setup>
  import axios from "axios";
- import { useRoute } from "vue-router";
- const route = useRoute();
- const getWeatherData = async () => {
-    try {
-        const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lon}&exclude={part}&appid=45a017e8e439461a792f7f024ad07b80
-        &units=imperial`);
+import { useRoute } from "vue-router";
 
-         // cal current date & time
-        const localOffset = new Date().getTimezoneOffset() * 60000;
-        const utc = weatherData.data.current.dt * 1000 + localOffset;
-        weatherData.data.currentTime =
+const route = useRoute();
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lon}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+    );
+
+    // cal current date & time
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = weatherData.data.current.dt * 1000 + localOffset;
+    weatherData.data.currentTime =
+      utc + 1000 * weatherData.data.timezone_offset;
+
+    // cal hourly weather offset
+    weatherData.data.hourly.forEach((hour) => {
+      const utc = hour.dt * 1000 + localOffset;
+      hour.currentTime =
         utc + 1000 * weatherData.data.timezone_offset;
+    });
 
-        // cal hourly weather offset
-        weatherData.data.hourly.forEach((hour) => {
-        const utc = hour.dt * 1000 + localOffset;
-        hour.currentTime =
-            utc + 1000 * weatherData.data.timezone_offset;
-        });
-
-        return weatherData.data;
-        
-    }catch (err) {
-        console.log(err);
-    }
- }
- const weatherData = await getWeatherData();
+    return weatherData.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+const weatherData = await getWeatherData();
 </script>
